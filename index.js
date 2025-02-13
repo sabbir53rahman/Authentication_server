502bde7ad7acaf1d26191d6866968cf4a2e9eab6
sabbir53rahman <sabbir53rahman@gmail.com>
2025-02-14 01:46:36 +0600

    done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://blog-site-v1cc.onrender.com",
     credentials: true,
   })
 );
